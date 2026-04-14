1bc61205dd2f6fdad91626637885b2d364496abc
Varun Kumar <primenemesis099@gmail.com>
2026-04-14 13:41:40 +0530

    Add files via upload

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          My name is Varun Kumar. I am a video editor.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
